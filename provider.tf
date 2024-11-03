c9e244493b698c9119600eb145b6a1a5251388ed
Tanmay Varade <42692334+tanmay6414@users.noreply.github.com>
2024-11-03 15:10:25 +0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.16.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.74.0"
+    }
+  }
+}
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+provider "aws" {
+  region = var.AWS_REGION
+}
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
